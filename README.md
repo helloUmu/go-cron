f065a35c66b60e5938ff4fe442ff8e355ba53757
achmad <achmadalfazari@gmail.com>
2023-05-21 20:46:29 +0700

    feat: add cron job to scrapping web in one time every day

---
+++ README.md
@@ -0,0 +1,3 @@
+# Go-Cron
+
+example of using cron jobs to scrapping website in one time every day
---
+++ model/model.go
@@ -0,0 +1,13 @@
+package model
+
+// Course stores information about a coursera course
+type Course struct {
+	Title       string
+	Description string
+	Creator     string
+	Level       string
+	URL         string
+	Language    string
+	Commitment  string
+	Rating      string
+}
